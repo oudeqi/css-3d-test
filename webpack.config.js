abe0d5c7a47dba62fcfc5710ecf5efd6b336fc95
oudeqi <496997374@qq.com>
2017-10-20 07:05:29 +0800

    **

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 
@@ -52,8 +52,6 @@ NODE_ENV === 'production' ? plugins.push(new UglifyJSPlugin({
     sourceMap: true
 })) : null;
 
-console.log(plugins);
-
 module.exports = {
     // context: path.resolve(__dirname, 'src'), //基本目录，一个绝对路径，用于从配置中解析入口点和装载器。
     devtool: 'source-map',
@@ -87,9 +85,13 @@ module.exports = {
             {
                 test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
                 loader: 'file-loader',
-                query: {
+                options: {
                     name: 'images/[name].[ext]',
-                    publicPath: PUBLIC_PATH
+                    // publicPath: PUBLIC_PATH
+                    // 单独设置图片资源的publicPath，其他文件保留output配置里的publicPath
+                    // 会覆盖 css里面的 图片的对外路径
+                    // 会影响 html里面 img标签的src属性
+                    // 会覆盖 output配置里的 publicPath
                 }
             }
         ]
